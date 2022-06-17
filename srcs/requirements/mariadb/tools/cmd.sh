d8b4536742a355ae9b6e97c58bb4b1712e108a6f
Aleandro Hernandez ontoria <ahernand@c2r15s6.42madrid.com>
2022-06-17 20:49:21 +0200

    began wordpress, lost time trying volumes and default wordpress image with mariadb

--- srcs/requirements/mariadb/tools/cmd.sh
+++ srcs/requirements/mariadb/tools/cmd.sh
@@ -4,7 +4,9 @@ mysql << EOF
 	create	database	$MARIADB_DATABASE;
 	create	user		'$MARIADB_USER' identified by '$MARIADB_PASSWORD';
 	grant all privileges on $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';
+	FLUSH PRIVILEGES;
 EOF
+
 
 service mysql stop
 
