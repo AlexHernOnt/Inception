c520b1f3c5a1c89cdff80315c4643a8db6d7ff21
Aleandro Hernandez ontoria <ahernand@c1r7s6.42madrid.com>
2022-07-18 18:15:08 +0200

    ssh not working

--- srcs/requirements/wordpress/conf/run.sh
+++ srcs/requirements/wordpress/conf/run.sh
@@ -1,4 +1,5 @@
+sleep 3
 #wp-cli core install --allow-root --path='var/www/html' --url=localhost --title="This is how you find the meaning of life" --admin_user=alex --admin_password=123 --admin_email=asd@asd.asd --force
-wp-cli core install --allow-root --url="localhost:443" --title="This is how you find the meaning of life" --admin_user=alex --admin_password=123 --admin_email=asd@asd.asd
 
+wp-cli core install --allow-root --path=`/var/www/html` --url=localhost:443 --title=life --admin_user=alex --admin_password=123 --admin_email=asd@asd.asd
 php-fpm7.3 -F
