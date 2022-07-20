ee6c80d1411c72b0482a6782967f0fb531a1e96e
Aleandro Hernandez ontoria <ahernand@c1r6s6.42madrid.com>
2022-07-20 14:36:57 +0200

    working???

--- srcs/requirements/wordpress/conf/run.sh
+++ srcs/requirements/wordpress/conf/run.sh
@@ -1,5 +1,5 @@
 sleep 3
 
-/usr/local/bin/wp-cli core install --allow-root --path=`/var/www/html` --url=localhost:443 --title=life --admin_user=alex --admin_password=123 --admin_email=asd@asd.asd
+#/usr/local/bin/wp-cli core install --allow-root --path=`/var/www/html` --url=localhost:443 --title=life --admin_user=alex --admin_password=123 --admin_email=asd@asd.asd
 
 php-fpm7.3 -F
